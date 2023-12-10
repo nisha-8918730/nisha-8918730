dfb36792e1d7c3744ef0edd38302a0ec7cc5cfe0
nisha8918730 <nisha190311@gmail.com>
2023-12-09 23:26:29 -0500

    Assignment 3 lab 5

--- assignment3/lab_5.py
+++ assignment3/lab_5.py
@@ -2,55 +2,60 @@
 
 # pip install selenium
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+
+
 import time
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as rc
 # Setting up the webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Navigating to the Amazon.ca homepage
 driver = webdriver.Chrome()
 
 # Navigating to the Amazon.ca homepage
-driver.get("https://www.amazon.ca")
+driver.get("https://www.w3schools.com/")
 time.sleep(3)
-
-# Finding the search bar and entering text
+wait = WebDriverWait(driver, 10)
+button = wait.until(rc.element_to_be_clickable((By.XPATH, "//*[@id='pagetop']/div[3]/a[1]")))
+button.click()
 # search_bar = driver.find_element_by_id("id","twotabsearchtextbox") old syntax
-search_bar = driver.find_element("id","twotabsearchtextbox")
-search_bar.send_keys("laptop")
-
-# Submitting the search query
-search_bar.send_keys(Keys.RETURN)
-
-# Waiting for the search results page to load
+logininput = driver.find_element(By.CSS_SELECTOR, "#modalusername")
+logininput.send_keys("nbains9911@gmail.com")
+passwordinput = driver.find_element(By.CSS_SELECTOR, "#current-password")
+passwordinput.send_keys("Emran*1999")
 time.sleep(5)
 
-# Verifying that the search results page has loaded
-assert "laptop" in driver.title
+Loginbutton = driver.find_element(By.CSS_SELECTOR, "#root > div > div > div:nth-child(4) > div.LoginModal_modal__1Yybs.LoginModal_show__F6L8A.LoginModal_full_page__PoJE0 > div > div.LoginModal_cta_bottom_box__wU1AF > div.LoginModal_cta_bottom_box_button_login__5Fbwv > button")
+Loginbutton.click()
+
+time.sleep(6)
+
+
+
+
+driver.close()
 
 # Selecting a laptop from the search results
-laptop_link = driver.find_element("xpath","/html/body/div[1]/div[1]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/div/div/div[1]/span/a/div/img")
-laptop_link.click()
 
 
 
-# Waiting for the laptop details page to load
-time.sleep(5)
+time.sleep(3)
 
-# Adding the laptop to the cart
-add_to_cart_button = driver.find_element("id","add-to-cart-button")
-add_to_cart_button.click()
+
+
+
+
+
+
 
 # Waiting for the cart to update
 time.sleep(5)
 
-# Clicking on no thanks button
-no_thanks_button= driver.find_element("xpath","/html/body/div[9]/div[3]/div[1]/div/div/div[2]/div[2]/div/div/div[3]/div/span[2]/span/input")
-no_thanks_button.click()
-time.sleep(2)
-
-proceed_to_checkout= driver.find_element("xpath","/html/body/div[1]/div[1]/div/div[1]/div[2]/div/div[3]/div/div[1]/form/span/span/span/input")
-proceed_to_checkout.click()
-time.sleep(2)
 
 
 # Verifying that the laptop has been added to the cart
